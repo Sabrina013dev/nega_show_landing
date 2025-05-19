1a05f8ba9373278b22e28f13442cda56ba0df370
Sabrina013dev <sabrina.oliveira0133@gmail.com>
2025-05-19 12:23:54 -0300

    melhora no carrosel

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,13 +1,27 @@
 AOS.init();
 
+// Configuração do carrossel infinito
 gsap.to(".testimonials", {
-    x: "-50%",
+    x: () => -(document.querySelector(".testimonials").scrollWidth - document.querySelector(".testimonials-wrapper").offsetWidth) + "px",
     ease: "none",
-    repeat: -1,
-    duration: 15,
+    duration: 20,
+    repeat: -1, // Loop infinito
     scrollTrigger: {
-        trigger: ".testimonials",
+        trigger: ".depoimentos",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 1 // Suaviza a animação com o scroll
+    }
+});
+
+// Efeito de rotação (opcional)
+gsap.from(".testimonial", {
+    rotationY: -15, // Pequena inclinação 3D
+    opacity: 0.8,
+    stagger: 0.2,
+    scrollTrigger: {
+        trigger: ".testimonials-wrapper",
         start: "top 80%",
-        toggleActions: "play pause resume reset"
+        toggleActions: "play none none none"
     }
 });
